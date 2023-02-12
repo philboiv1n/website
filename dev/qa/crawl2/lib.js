bc894bd3cfdf1364d1906039ada3ae569e74c881
Phil Boivin <pboivin78@gmail.com>
2023-02-12 17:26:34 -0500

    Crawl test

--- dev/qa/crawl2/lib.js
+++ dev/qa/crawl2/lib.js
@@ -1,48 +1,23 @@
-const request = new XMLHttpRequest();
-const visitedUrls = new Set();
-let activeRequests = 0;
-const MAX_ACTIVE_REQUESTS = 100;
-
-function getUrls(baseUrl) {
-  const urls = [];
-  function crawl(url) {
-    if (visitedUrls.has(url)) {
-      return;
+function getLinks(url) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(this.responseText, "text/html");
+      const links = htmlDoc.getElementsByTagName("a");
+      const urls = [];
+      for (let i = 0; i < links.length; i++) {
+        urls.push(links[i].href);
+      }
+      console.log(JSON.stringify({ urls: urls }));
+      let table = "<table><tr><th>URL</th></tr>";
+      urls.forEach(url => {
+        table += `<tr><td>${url}</td></tr>`;
+      });
+      table += "</table>";
+      document.getElementById("tableContainer").innerHTML = table;
     }
-    visitedUrls.add(url);
-    urls.push(url);
-    activeRequests++;
-    request.open("GET", url, true);
-    request.onreadystatechange = function() {
-      activeRequests--;
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        const parser = new DOMParser();
-        const htmlDoc = parser.parseFromString(this.responseText, "text/html");
-        const links = htmlDoc.getElementsByTagName("a");
-        for (let i = 0; i < links.length; i++) {
-          const link = links[i];
-          if (link.href.startsWith(baseUrl) && activeRequests < MAX_ACTIVE_REQUESTS) {
-            crawl(link.href);
-          }
-        }
-      }
-      if (activeRequests === 0) {
-        console.log(JSON.stringify({ urls: Array.from(urls) }));
-        let table = "<table><tr><th>URL</th></tr>";
-        urls.forEach(url => {
-          table += `<tr><td>${url}</td></tr>`;
-        });
-        table += "</table>";
-        document.getElementById("tableContainer").innerHTML += table;
-      }
-    };
-    request.send();
-  }
-
-  visitedUrls.clear();
-  activeRequests = 0;
-  crawl(baseUrl);
+  };
+  request.send();
 }
-
-getUrls("https://www.philboivin.com/");
-getUrls("https://www.philboivin.com/fr/photos.html");
